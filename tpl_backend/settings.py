d9eb79ec5f824f214af8e05b7da0763b4f5a7f47
Tudor Andronic <tudorandronic@Tudors-MacBook-Pro.local>
2022-04-11 19:00:21 +0300

    Updated settings

--- tpl_backend/settings.py
+++ tpl_backend/settings.py
@@ -149,7 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
